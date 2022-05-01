fbb63b4b7d2cd19156029ee5cf04bb2f0e7919c2
aymenkhs <aymenkhouas@gmail.com>
2022-05-01 18:38:53 +0100

    [feat] init developped_models module

---
+++ summarization_models/developped_models.py
@@ -0,0 +1,13 @@
+import torch
+
+def pegasus(data, model_path):
+    pass
+
+def distilled_pegasus(data, model_path):
+    pass
+
+def distilled_bart(data, model_path):
+    pass
+
+def transformers(data, model_path):
+    pass
