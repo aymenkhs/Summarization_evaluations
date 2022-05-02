909d2939e15429051a9830b09f49edaae603b283
aymenkhs <aymenkhouas@gmail.com>
2022-05-02 14:02:24 +0100

    [add] add distilled bart and pegasus functions to execute the model

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from summarization_models import api_models, developped_models
 
 def main():
     test_data = utils.load_data()
-    developped_models.pegasus(test_data)
+    developped_models.distilled_bart(test_data)
 
 
 if __name__ == '__main__':
--- summarization_models/developped_models.py
+++ summarization_models/developped_models.py
@@ -1,5 +1,6 @@
 import torch
 from transformers import PegasusTokenizer, PegasusForConditionalGeneration
+from transformers import BartTokenizer, BartForConditionalGeneration
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
@@ -21,11 +22,41 @@ def pegasus(data, model_path='models/pegasus_75000_steps', model_name='google/pe
     data['result_summary'] = results
     return data
 
-def distilled_pegasus(data, model_path):
-    pass
+def distilled_pegasus(data, model_path='models/distill_pegasus_30_epochs', model_name='sshleifer/distill-pegasus-xsum-16-4'):
 
-def distilled_bart(data, model_path):
-    pass
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_path)
+
+    results = []
+    for instance in data.index:
+        source = data.loc[instance]['dialogue']
+        tokens = tokenizer(source, truncation=True, padding="longest", return_tensors="pt").to(device)
+        summary = model.generate(**tokens)
+        summary = tokenizer.batch_decode(summary, skip_special_tokens=True)
+        results += summary
+        print(summary)
+        import pdb; pdb.set_trace()
+
+    data['result_summary'] = results
+    return data
+
+def distilled_bart(data, model_path='models/distill_bart_1_1', model_name='sshleifer/distilbart-xsum-1-1'):
+
+    tokenizer = BartTokenizer.from_pretrained(model_name)
+    model = BartForConditionalGeneration.from_pretrained(model_path)
+
+    results = []
+    for instance in data.index:
+        source = data.loc[instance]['dialogue']
+        tokens = tokenizer(source, truncation=True, padding="longest", return_tensors="pt").to(device)
+        summary = model.generate(**tokens)
+        summary = tokenizer.batch_decode(summary, skip_special_tokens=True)
+        results += summary
+        print(summary)
+        import pdb; pdb.set_trace()
+
+    data['result_summary'] = results
+    return data
 
 def transformers(data, model_path):
     pass
